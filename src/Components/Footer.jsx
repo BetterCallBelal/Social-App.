339556ced2800e3d77b8026062a39b2ed5f09b67
Belal Adel <116830786+BetterCallBelal@users.noreply.github.com>
2026-08-18 07:19:01 -0900

    refactor: update Footer component structure and formatting

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    
+    <footer
+      className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+      <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
+        © 2023 Copyright:
+        <a
+          className="text-neutral-800 dark:text-neutral-400"
+          href="https://tw-elements.com/"
+        >TW Elements</a>
+      </div>
+    </footer>
   )
 }
